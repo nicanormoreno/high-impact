7ff1016d6c258ce8471e461d2e031ec4223e198e
nicanormoreno <nicanormorenodipietro@gmail.com>
2020-02-08 12:35:20 -0300

    config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './screens/home/home.component';
+import { LoginComponent } from './screens/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent, canActivate:[AuthGuard]},
+  {path: 'login', component:LoginComponent},
+  {path:'**', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const screens=[
+  HomeComponent,
+  LoginComponent
+]
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate,Router } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router:Router){}
+
+  canActivate():boolean {
+    localStorage.removeItem('logged')
+    this.router.navigateByUrl('/login')
+    return false
+  }
+  
+}
